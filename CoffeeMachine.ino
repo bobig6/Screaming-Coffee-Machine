c315fc72bba03191b503d5696c3fe9892bdc70df
bobig6 <b.i.ivanov1@gmail.com>
2020-06-06 10:27:22 +0300

    Update CoffeeMachine.ino

--- CoffeeMachine.ino
+++ CoffeeMachine.ino
@@ -8,19 +8,20 @@ TM1637Display display = TM1637Display(CLK, DIO);
 
 // Init the DS3231 using the hardware interface
 DS3231  rtc(SDA, SCL);
+
+//water sensor pin
+int waterPin = 33;
 //esp pin
 const int espPin = 32;
-// ultrasonic pins
-const int trigPin = 3;
-const int echoPin = 2;
 //relay pins
 const int relayPin = 10;
-// defines variables for sensors
-long duration;
-int distance;
 //button pins
-int but1Pin = 30;
-int but2Pin = 31;
+int but1Pin = 31;
+int but2Pin = 30;
+int CupButton = 45;
+
+//water sensor variables
+unsigned long duration = 10000;
 //button variables
 int buttonPushCounter1 = 0;   // counter for the number of button presses
 int buttonState1 = 0;         // current state of the button
@@ -29,6 +30,7 @@ int lastButtonState1 = 0;     // previous state of the button
 int buttonPushCounter2 = 0;   // counter for the number of button presses
 int buttonState2 = 0;         // current state of the button
 int lastButtonState2 = 0;     // previous state of the button
+
 //clock time
 int timerHours = 17;
 int timerMinutes = 0;
@@ -40,23 +42,23 @@ void setup()
   // IStarts the clock module
   rtc.begin();
   
-  //sets sensor pins
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   //sets relay pins
   pinMode(relayPin, OUTPUT);
+  //sets waterSensorPins
+  pinMode(waterPin, INPUT);
   //sets buttons pins
   pinMode(but1Pin, INPUT);
   pinMode(but2Pin, INPUT);
+  pinMode(CupButton, INPUT);
   //sets espPin
   pinMode(espPin, INPUT);
   //sets default value to off to relays
-  digitalWrite(relayPin, HIGH);
+  digitalWrite(relayPin, LOW);
   // Clear the display:
   display.clear();
   // The following lines can be uncommented to set the date and time
   rtc.setDOW(SUNDAY);     // Set Day-of-Week to SUNDAY
-  rtc.setTime(17, 00, 00);     // Set the time to 12:00:00 (24hr format)
+  rtc.setTime(17, 05, 00);     // Set the time to 12:00:00 (24hr format)
   rtc.setDate(29, 12, 2019);   // Set the date to January 1st, 2014
 }
 
@@ -87,7 +89,7 @@ void loop()
   //hours = hours*100 + minutes;
 
 
-  CalculateDistance();
+
 
 
 /*
@@ -141,27 +143,35 @@ void loop()
   
   //logic
   if((hours == timerHours && minutes == timerMinutes) || (digitalRead(espPin) == HIGH)){
-    if(distance <= 5){
-      digitalWrite(relayPin, LOW);
-      delay(3000);
-      Serial.println("I am making cofee!!!");
-      int seconds_counter = 0;
-      while(distance <= 5){
-        CalculateDistance();
-        delay(1000);
-        seconds_counter++;
-        if(seconds_counter == 10){
-          Serial.println("Your coffee will get cold!");
+    if(digitalRead(CupButton) == HIGH){
+      delay(2000);
+      if(digitalRead(CupButton) == HIGH){
+        digitalWrite(relayPin, HIGH);
+        duration = pulseIn(waterPin, HIGH);
+        Serial.println("I am making cofee!!!");
+        while(duration <= 900){
+          duration = pulseIn(waterPin, HIGH);
+          Serial.println(duration);
         }
-        if(seconds_counter == 20){
-          Serial.println("Why did I make this coffee!");
-        }
-        if(seconds_counter >= 30){
-          Serial.println("Take your coffee!");
+        digitalWrite(relayPin, LOW);  
+        int seconds_counter = 0;
+        while(digitalRead(CupButton) == HIGH){
           delay(1000);
+          seconds_counter++;
+          if(seconds_counter == 10){
+            Serial.println("Your coffee will get cold!");
+          }
+          if(seconds_counter == 20){
+            Serial.println("Why did I make this coffee!");
+          }
+          if(seconds_counter >= 30){
+            Serial.println("Take your coffee!");
+            delay(1000);
+          }
         }
       }
-      digitalWrite(relayPin, HIGH);  
+      
+      
     }
   }
   
@@ -171,52 +181,3 @@ void loop()
   // Wait one second before repeating :)
   //delay (1000);
 }
-
-
-
-void CalculateDistance(){
-  //calculate distance on sensor 1
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance= duration*0.034/2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-}
-
-/*
-//Not implemented function for fast count if held
-int Pushbutton(){
-  if (millis() - buttontimeout >20){   //only read the analog line every 20ms. I had problems reading it more frequently.
-   buttontimeout = millis();
-   leftbuttons = analogRead(A5);
-   
-    button = 0;
-    if (leftbuttons <785 && leftbuttons>745) button = 1; //LH Button 1 is pushed
-    if (leftbuttons <630 && leftbuttons>590) button = 2; //LH Button 2 is pushed
-     
-    if (button != oldbutton){
-      oldbutton = button;
-      if ( button !=0){
-      repeatbutton = millis();
-      return button; 
-      }
-    }
-   
-   
-
-  if (millis()- repeatbutton > 500){
-     repeatbutton = millis();
-     return button;
-     
-   }
-  }
-}
-*/
